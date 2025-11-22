daf99b14b66d5ae7b3d8e1ba3d9342789b2a81e7
Francesco Bazaj <150333738+JurijGagarinFB@users.noreply.github.com>
2025-11-22 11:27:15 +0100

    sus

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+echo "Hello World!";
+echo "<br>";
+echo "<br>";
+
+
+echo "buongiorno a tutti";
+echo "<br>";
+echo "<br>";
+
+
+$var = 10;
+echo "il valore della variabile è: ".$var;
+echo "<br>";
+var_dump($var);
+echo "<br>";
+echo "<br>";
+
+
+$var2 = 2.50;
+echo "il valore della variabile è: ".$var2;
+echo "<br>";
+var_dump($var2);
+echo "<br>";
+echo "<br>";
+
+
+$str = "test";
+echo "il valore della variabile è: ".$str;
+echo "<br>";
+var_dump($str);
+echo "<br>";
+echo "<br>";
